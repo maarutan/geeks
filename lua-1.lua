54f25e1c12e8d5ba73d013e2133887a8a64f62e2
Marat Arzymatov <147023214+maarutan@users.noreply.github.com>
2024-10-03 17:29:59 +0600

    Add files via upload

---
+++ lua-1.lua
@@ -0,0 +1,16 @@
+-- обявления переменной
+
+local x = 10 -- number
+local isAlive = true -- bool
+local sayHello = 'helo' -- string 
+local a = nil -- нет значения или недопустимое значение 
+
+
+local n = 1
+n = n + 1 
+-- print(n) -- 2
+
+local name = "marat"
+local suranme = "arzymatov"
+
+print(name .. " " .. suranme)
