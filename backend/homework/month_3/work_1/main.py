85542443f25310d30dbf972125de7eecdb054ba0
maaru <maratarzymatov288@gmail.com>
2024-11-04 16:51:33 +0600

    переписал с урока

--- backend/homework/month_3/work_1/main.py
+++ backend/homework/month_3/work_1/main.py
@@ -1,4 +1,27 @@
 from decouple import Config, RepositoryEnv  # type: ignore
+from aiogram import Bot, Dispatcher, executor, types  # type: ignore
+import logging
+
+
 config = Config(RepositoryEnv("../.env"))
-TOKEN = config("TOKEN")
-print(TOKEN)               
+token = config("TOKEN")
+bot = Bot(token=token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await bot.send_message(
+        chat_id=message.from_user.id,
+        text=f"Hello! {message.from_user.first_name}\n"
+        f"your Telegram id = {message.from_user.id}",
+    )
+    await bot.send_message(
+        chat_id=message.from_user.id,
+        text=f"че как дела у вас {message.from_user.first_name}\n",
+    )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True)
